fa49e2b0018b515195b2589a285a79d1a4d5689e
jjaymen <githubaymen@gmail.com>
2026-03-31 12:16:03 +0200

    final merge, modified es.py

--- es.py
+++ es.py
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 # ciao, aymengr membro 1, mega membro 2, rikyone membro 3
 
 def saluta(nome):
     return f"Ciao, {nome}!"
+
 # modifica del meguz
 def somma(a, b):
     return a + b
 
 def saluta(nome):
-    return f"Ciao, {nome}!"
-# modifica del meguz
-def somma(a, b):
-    return a + b
-=======
-def saluta(nome):
     return f"Ciao {nome}, sei negro"
->>>>>>> origin/richiemeggio
